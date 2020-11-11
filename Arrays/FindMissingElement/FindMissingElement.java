cedfa3b68066e022e4365c33417e4eb42ddfe6de
alpha037 <subhronil.dutta@gmail.com>
2020-11-12 01:08:14 +0530

    Add: Find Missing Element

---
+++ Arrays/FindMissingElement/FindMissingElement.java
@@ -0,0 +1,30 @@
+package Arrays.FindMissingElement;
+
+public class FindMissingElement {
+
+  public static int finder(int[] array, int[] shuffledArray) {
+
+    // * This could cause an overflow if the numbers are too big or too small or
+    // * if the array is too long.
+
+    // int sum = array[0];
+
+    // for (int i = 1; i < array.length; i++)
+    // sum += array[i];
+
+    // for (int i = 0; i < shuffledArray.length; i++)
+    // sum -= shuffledArray[i];
+
+    // * XOR-ing a number with itself is -> 0,
+    // * XOR-ing a number with 0 is -> the number itself
+    int sum = 0;
+
+    for (int el : array)
+      sum ^= el;
+
+    for (int el : shuffledArray)
+      sum ^= el;
+
+    return sum;
+  }
+}
---
+++ Arrays/FindMissingElement/FindMissingElementTest.java
@@ -0,0 +1,23 @@
+package Arrays.FindMissingElement;
+
+public class FindMissingElementTest {
+  public static void main(String[] args) {
+
+    // should be 5
+    System.out.println(FindMissingElement.finder(new int[] { 1, 2, 3, 4, 5, 6, 7, }, new int[] { 3, 7, 2, 1, 4, 6 }));
+
+    // should be 145
+    System.out.println(
+        FindMissingElement.finder(new int[] { 98, 99, 100, 120, 158, 145 }, new int[] { 120, 158, 98, 99, 100 }));
+
+    // should be 548545154
+    System.out.println(
+        FindMissingElement.finder(new int[] { 988888889, 989898578, 458713549, 488752554, 548545154, 548488554 },
+            new int[] { 989898578, 458713549, 548488554, 988888889, 488752554 }));
+
+    // should be 6
+    System.out.println(
+        FindMissingElement.finder(new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 }, new int[] { 9, 8, 7, 5, 4, 3, 2, 1 }));
+
+  }
+}
