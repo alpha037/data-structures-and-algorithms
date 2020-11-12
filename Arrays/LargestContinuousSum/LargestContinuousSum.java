47bdf8eaf8761aa1080901f83b485effa940bb1b
alpha037 <subhronil.dutta@gmail.com>
2020-11-13 00:52:10 +0530

    Add: Largest Continuous Sum

---
+++ Arrays/LargestContinuousSum/LargestContinuousSum.java
@@ -0,0 +1,24 @@
+package Arrays.LargestContinuousSum;
+
+public class LargestContinuousSum {
+
+  public static int find(int[] array) {
+
+    // * Edge Case
+    if (array.length == 0)
+      return 0;
+
+    int maxSum = array[0];
+    int currentSum = array[0];
+
+    // * Kadane's Algorithm
+    for (int i = 1; i < array.length; i++) {
+
+      currentSum = Math.max(currentSum + array[i], array[i]);
+
+      maxSum = Math.max(currentSum, maxSum);
+    }
+
+    return maxSum;
+  }
+}
