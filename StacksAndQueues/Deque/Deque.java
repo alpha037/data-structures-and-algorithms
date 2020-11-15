aea80cbe57166cdc975ba59aaf8d03dc7b72a7d0
alpha037 <subhronil.dutta@gmail.com>
2020-11-15 23:16:36 +0530

    Add: Deque implementation

---
+++ StacksAndQueues/Deque/Deque.java
@@ -0,0 +1,77 @@
+package StacksAndQueues.Deque;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deque {
+
+  private List<Integer> list;
+
+  /**
+   * Constructor for the Deque
+   */
+  public Deque() {
+    this.list = new ArrayList<>();
+  }
+
+  /**
+   * Pushes an item into the front of the deque
+   * 
+   * @param item : The element to be pushed into the deque
+   */
+  public void addFront(int item) {
+    this.list.add(0, item);
+  }
+
+  /**
+   * Pushes an item into the rear of the deque
+   * 
+   * @param item : The element to be pushed into the deque
+   */
+  public void addRear(int item) {
+    this.list.add(item);
+  }
+
+  /**
+   * Returns and removes the first item from the front of the deque
+   * 
+   * @return An integer value containing the first item from the deque after
+   *         removing it
+   */
+  public int removeFront() {
+    return this.list.remove(0);
+  }
+
+  /**
+   * Returns and removes the last item from the rear of the deque
+   * 
+   * @return An integer value containing the last item from the deque after
+   *         removing it
+   */
+  public int removeRear() {
+    return this.list.remove(this.list.size() - 1);
+  }
+
+  /**
+   * Returns true if the deque is empty, otherwise false
+   * 
+   * @return A boolean value determining if the deque is empty or not
+   */
+  public boolean isEmpty() {
+    return this.list.isEmpty();
+  }
+
+  /**
+   * The current size of the deque
+   * 
+   * @return A boolean value containing the current size of the deque
+   */
+  public int size() {
+    return this.list.size();
+  }
+
+  @Override
+  public String toString() {
+    return this.list.toString();
+  }
+}
---
+++ StacksAndQueues/Deque/DequeTest.java
@@ -0,0 +1,46 @@
+package StacksAndQueues.Deque;
+
+public class DequeTest {
+  public static void main(String[] args) {
+
+    Deque deque = new Deque();
+    System.out.println("Deque created.");
+
+    // should be 0
+    System.out.println("\nSize of the deque: " + deque.size());
+
+    System.out.println("\nAdding items...");
+    deque.addFront(10);
+    deque.addFront(20);
+    deque.addFront(30);
+    deque.addRear(40);
+    deque.addRear(50);
+    deque.addRear(60);
+    deque.addFront(70);
+    deque.addRear(80);
+
+    // should be 70, 30, 20, 10, 40, 50, 60, 80
+    System.out.println("\nQueue items: " + deque.toString());
+
+    // should be 8
+    System.out.println("\nSize of the deque: " + deque.size());
+
+    System.out.println("\nRemoving items from the front...");
+    System.out.println(deque.removeFront()); // should be 70
+    System.out.println(deque.removeFront()); // should be 30
+    System.out.println(deque.removeFront()); // should be 20
+
+    // should be 5
+    System.out.println("\nSize of the deque: " + deque.size());
+
+    System.out.println("\nRemoving items from the rear...");
+    System.out.println(deque.removeRear()); // should be 80
+    System.out.println(deque.removeRear()); // should be 60
+    System.out.println(deque.removeRear()); // should be 50
+    System.out.println(deque.removeRear()); // should be 40
+    System.out.println(deque.removeRear()); // should be 10
+
+    // should be true
+    System.out.println("\nIs the deque empty? " + deque.isEmpty());
+  }
+}
