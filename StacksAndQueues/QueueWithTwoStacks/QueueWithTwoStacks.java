155a480cf0093dc676036cf7ea10cc0aa9a4e2d8
alpha037 <subhronil.dutta@gmail.com>
2020-11-16 21:01:48 +0530

    Add: Implement queue using two stacks

---
+++ StacksAndQueues/QueueWithTwoStacks/QueueWithTwoStacks.java
@@ -0,0 +1,41 @@
+package StacksAndQueues.QueueWithTwoStacks;
+
+import java.util.Stack;
+
+public class QueueWithTwoStacks<T> {
+
+  private Stack<T> inStack;
+  private Stack<T> outStack;
+
+  public QueueWithTwoStacks() {
+    this.inStack = new Stack<>();
+    this.outStack = new Stack<>();
+  }
+
+  private void reverseOrder() {
+    if (!this.outStack.isEmpty())
+      return;
+
+    System.out.println("\nReversing the order...");
+
+    for (int i = 0; i < inStack.size();)
+      this.outStack.push(this.inStack.pop());
+  }
+
+  public void enqueue(T item) {
+    this.inStack.push(item);
+  }
+
+  public T dequeue() {
+    this.reverseOrder();
+    return this.outStack.pop();
+  }
+
+  public boolean isEmpty() {
+    return this.inStack.isEmpty() == this.outStack.isEmpty();
+  }
+
+  public int size() {
+    return !this.inStack.isEmpty() ? this.inStack.size() : this.outStack.size();
+  }
+}
---
+++ StacksAndQueues/QueueWithTwoStacks/QueueWithTwoStacksTest.java
@@ -0,0 +1,42 @@
+package StacksAndQueues.QueueWithTwoStacks;
+
+public class QueueWithTwoStacksTest {
+  public static void main(String[] args) {
+    QueueWithTwoStacks<Integer> queue = new QueueWithTwoStacks<>();
+    System.out.println("Queue created.");
+
+    // should be 0
+    System.out.println("\nSize of the queue: " + queue.size());
+
+    System.out.println("\nEnqueueing items...");
+    queue.enqueue(10);
+    queue.enqueue(20);
+    queue.enqueue(30);
+    queue.enqueue(40);
+    queue.enqueue(50);
+    queue.enqueue(60);
+    queue.enqueue(70);
+    queue.enqueue(80);
+
+    // should be 8
+    System.out.println("\nSize of the queue: " + queue.size());
+
+    System.out.println("\nDequeueing items...");
+    System.out.println(queue.dequeue()); // should be 10
+    System.out.println(queue.dequeue()); // should be 20
+    System.out.println(queue.dequeue()); // should be 30
+
+    // should be 5
+    System.out.println("\nSize of the queue: " + queue.size());
+
+    System.out.println("\nDequeueing some more items...");
+    System.out.println(queue.dequeue()); // should be 40
+    System.out.println(queue.dequeue()); // should be 50
+    System.out.println(queue.dequeue()); // should be 60
+    System.out.println(queue.dequeue()); // should be 70
+    System.out.println(queue.dequeue()); // should be 80
+
+    // should be true
+    System.out.println("\nIs the queue empty? " + queue.isEmpty());
+  }
+}
