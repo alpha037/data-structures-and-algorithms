ac47c5aa23183f00cc04efd092799f527509e30d
alpha037 <subhronil.dutta@gmail.com>
2020-11-21 21:04:50 +0530

    Add: Factorial with Recursion and Memoization

---
+++ Recursion/Factorial/Factorial.java
@@ -0,0 +1,45 @@
+package Recursion.Factorial;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Factorial {
+
+  private static Map<Integer, Integer> map = new HashMap<>();
+
+  public static enum TYPE {
+    RECURSIVE,
+    MEMOIZATION
+  };
+
+  private static int recursiveFactorial(int n) {
+    if (n < 2)
+      return 1;
+
+    return n * recursiveFactorial(n - 1);
+  }
+
+  private static int memoizedFactorial(int n) {
+    if (n < 2)
+      return 1;
+    
+    if (!map.containsKey(n))
+      map.put(n, n * memoizedFactorial(n - 1));
+
+    return map.get(n);
+  }
+
+  public static int factorial(int n, TYPE type) {
+    switch (type) {
+      case RECURSIVE: 
+        return recursiveFactorial(n);
+
+      case MEMOIZATION: 
+        return memoizedFactorial(n);
+
+      default: 
+        System.out.println("Invalid type.");
+        return -1;
+    }
+  }
+}
---
+++ Recursion/Factorial/FactorialTest.java
@@ -0,0 +1,17 @@
+package Recursion.Factorial;
+
+import Recursion.Factorial.Factorial.TYPE;
+
+public class FactorialTest {
+  public static void main(String[] args) {
+    
+    // should be 720
+    System.out.println(Factorial.factorial(6, TYPE.RECURSIVE));
+
+    // should be 720
+    System.out.println(Factorial.factorial(6, TYPE.MEMOIZATION));
+    
+    // should be 5040 (should take less time as 6! is already calculated)
+    System.out.println(Factorial.factorial(7, TYPE.MEMOIZATION));
+  }
+}
