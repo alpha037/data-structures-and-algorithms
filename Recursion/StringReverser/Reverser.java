83c26b384f1c6bf0d5bfd06e4ccf6e098cbf8241
alpha037 <subhronil.dutta@gmail.com>
2020-11-21 22:17:47 +0530

    Add: Reverse a string using recusrion

---
+++ Recursion/StringReverser/Reverser.java
@@ -0,0 +1,15 @@
+package Recursion.StringReverser;
+
+public class Reverser {
+
+  public static String reverse(String string) {
+
+    // * Base Case
+    if (string.length() <= 1)
+      return string;
+
+    return 
+      string.charAt(string.length() - 1) + 
+      reverse(string.substring(0, string.length() - 1));
+  }
+}
