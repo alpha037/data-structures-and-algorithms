5498650ec9d9ea24fec99730d86c61382b2e0c63
alpha037 <subhronil.dutta@gmail.com>
2020-11-22 01:07:31 +0530

    Add: Permutations of string recursively

---
+++ Recursion/Permutation/Permutation.java
@@ -0,0 +1,32 @@
+package Recursion.Permutation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutation {
+
+  private static List<String> permutations = new ArrayList<>();
+
+  private static List<String> permute(String string, String res) {
+
+    // * Base Case
+    if (string.length() == 0)
+      permutations.add(res);
+
+    for (int i=0; i<string.length(); i++)
+      permute(
+        string.substring(0, i) + string.substring(i + 1),
+        res + string.charAt(i)
+      );
+
+    return permutations;
+  }
+
+  public static List<String> permute(String string) {
+    return permute(string, "");
+  }
+
+  public static void clearList() {
+    permutations.clear();
+  }
+}
