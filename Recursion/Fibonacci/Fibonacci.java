b486a5f493d4b2ca1bffeefece2f73281f19106d
alpha037 <subhronil.dutta@gmail.com>
2020-11-22 02:08:09 +0530

    Add: Fibonacci Sequence in 3 different ways

---
+++ Recursion/Fibonacci/Fibonacci.java
@@ -0,0 +1,77 @@
+package Recursion.Fibonacci;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Fibonacci {
+
+  private static Map<Integer, Integer> map = new HashMap<>();
+
+  public static enum TYPE {
+    RECURSION,
+    MEMOIZATION,
+    ITERATION,
+  }
+
+  public static int fibo(int n, TYPE type) {
+    switch (type) {
+
+      case RECURSION:
+        return fibonacciByRecursion(n);
+
+      case MEMOIZATION:
+        return fibonacciByMemoization(n);
+
+      case ITERATION:
+        return fibonacciByIteration(n);
+
+      default:
+        System.err.println("Invalid type");
+        return -1;
+    }
+  }
+
+
+  private static int fibonacciByRecursion(int n) {
+    
+    // * Base Case
+    if (n <= 1)
+      return n;
+
+    return 
+      fibonacciByRecursion(n - 1) + 
+      fibonacciByRecursion(n - 2);
+  }
+
+  private static int fibonacciByMemoization(int n) {
+
+    // * Base Case
+    if (n <= 1)
+      return n;
+
+    if (!map.containsKey(n))  
+      map.put(
+        n,
+        fibonacciByMemoization(n - 1) +
+        fibonacciByMemoization(n - 2)
+      );
+
+    return map.get(n);
+  }
+
+  private static int fibonacciByIteration(int n) {
+
+    if (n <= 2)
+      return 1;
+
+    int n1 = 0, n2 = 1, sum = 0;
+
+    for (int i = 2; i <= n; i++) {
+      sum = n1 + n2;
+      n1 = n2;
+      n2 = sum;
+    }
+    
+    return sum;
+  }
+}
---
+++ Recursion/Fibonacci/FibonacciTest.java
@@ -0,0 +1,68 @@
+package Recursion.Fibonacci;
+
+import Recursion.Fibonacci.Fibonacci.TYPE;
+
+public class FibonacciTest {
+  public static void main(String[] args) {
+    
+    // should be 55
+    System.out.println(
+      TYPE.RECURSION + " -> " +
+      Fibonacci.fibo(10, TYPE.RECURSION)
+    );
+
+    // should be 55
+    System.out.println(
+      TYPE.MEMOIZATION + " -> " +
+      Fibonacci.fibo(10, TYPE.MEMOIZATION)
+    );
+
+    // should be 55
+    System.out.println(
+      TYPE.ITERATION + " -> " +
+      Fibonacci.fibo(10, TYPE.ITERATION)
+    );
+    System.out.println();
+    
+    
+    // should be 1
+    System.out.println(
+      TYPE.RECURSION + " -> " +
+      Fibonacci.fibo(1, TYPE.RECURSION)
+    );
+
+    // should be 1
+    System.out.println(
+      TYPE.MEMOIZATION + " -> " +
+      Fibonacci.fibo(1, TYPE.MEMOIZATION)
+    );
+
+    // should be 1
+    System.out.println(
+      TYPE.ITERATION + " -> " +
+      Fibonacci.fibo(1, TYPE.ITERATION)
+    );
+    System.out.println();
+    
+    
+    
+    // should be 28657
+    System.out.println(
+      TYPE.RECURSION + " -> " +
+      Fibonacci.fibo(23, TYPE.RECURSION)
+    );
+
+    // should be 28657
+    System.out.println(
+      TYPE.MEMOIZATION + " -> " +
+      Fibonacci.fibo(23, TYPE.MEMOIZATION)
+    );
+
+    // should be 28657
+    System.out.println(
+      TYPE.ITERATION + " -> " +
+      Fibonacci.fibo(23, TYPE.ITERATION)
+    );
+    System.out.println();
+  }
+}
