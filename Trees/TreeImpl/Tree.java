969df68e5db73bfb3c5b84460bf998e334a2c4c1
alpha037 <subhronil.dutta@gmail.com>
2020-11-25 22:02:32 +0530

    Add: Tree implementation

---
+++ Trees/TreeImpl/Tree.java
@@ -0,0 +1,48 @@
+package Trees.TreeImpl;
+
+public class Tree {
+
+  private int value;
+  private Tree left;
+  private Tree right;
+
+  public Tree(int value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  public void insertLeft(Tree newNode) {
+    if (this.left == null)
+      this.left = newNode;
+    else {
+      newNode.left = this.left;
+      this.left = newNode;
+    }
+  }
+
+  public void insertRight(Tree newNode) {
+    if (this.right == null)
+      this.right = newNode;
+    else {
+      newNode.right = this.right;
+      this.right = newNode;
+    }
+  }
+
+  public Tree getLeftChild() {
+    return this.left;
+  }
+
+  public Tree getRightChild() {
+    return this.right;
+  }
+
+  public int getNodeValue() {
+    return this.value;
+  }
+
+  public void setNodeValue(int value) {
+    this.value = value;
+  }
+}
---
+++ Trees/TreeImpl/TreeTest.java
@@ -0,0 +1,60 @@
+package Trees.TreeImpl;
+
+public class TreeTest {
+  public static void main(String[] args) {
+    Tree root = new Tree(10);
+
+    root.insertLeft(new Tree(40));
+    root.insertLeft(new Tree(30));
+    root.insertLeft(new Tree(20));
+
+    root.insertRight(new Tree(70));
+    root.insertRight(new Tree(60));
+    root.insertRight(new Tree(50));
+
+    // root value -> 10
+    System.out.println("Root -> " + root.getNodeValue());
+    
+    // left subtree values
+    System.out.println("\nLeft Subtree: ");
+    System.out.println(
+      root
+        .getLeftChild()
+        .getNodeValue()
+    );
+    System.out.println(
+      root
+        .getLeftChild()
+        .getLeftChild()
+        .getNodeValue()
+    );
+    System.out.println(
+      root
+        .getLeftChild()
+        .getLeftChild()
+        .getLeftChild()
+        .getNodeValue()
+    );
+    
+    // right subtree values
+    System.out.println("\nRight Subtree: ");
+    System.out.println(
+      root
+        .getRightChild()
+        .getNodeValue()
+    );
+    System.out.println(
+      root
+        .getRightChild()
+        .getRightChild()
+        .getNodeValue()
+    );
+    System.out.println(
+      root
+        .getRightChild()
+        .getRightChild()
+        .getRightChild()
+        .getNodeValue()
+    );
+  }
+}
