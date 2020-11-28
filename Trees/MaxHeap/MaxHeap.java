c447f7316676fa32152cfa4881c86f345d388192
alpha037 <subhronil.dutta@gmail.com>
2020-11-28 18:10:46 +0530

    Add: Binary heap implementation - Max heap

---
+++ Trees/MaxHeap/MaxHeap.java
@@ -0,0 +1,148 @@
+package Trees.MaxHeap;
+
+import java.util.Arrays;
+
+public class MaxHeap {
+  
+  private int capacity;
+  private int size;
+  private int[] arr;
+
+  public MaxHeap(int capacity) {
+    this.capacity = capacity;
+    this.size = 0;
+    this.arr = new int[capacity];
+  }
+
+  public MaxHeap(int capacity, int[] arr) {
+    this.capacity = capacity;
+    this.size = arr.length;
+    this.arr = new int[capacity];
+    
+    fillArray(arr);
+    buildHeap();
+  }
+
+  public int size() {
+    return this.size;
+  }
+
+  public int capacity() {
+    return this.capacity;
+  }
+
+  // * ----- Private Helper Methods -----
+
+  private int left(int i) {
+    return (int) Math.floor((2 * i) + 1);
+  }
+
+  private int right(int i) {
+    return (int) Math.floor((2 * i) + 2);
+  }
+
+  private int parent(int i) {
+    return (int) Math.floor((i - 1) / 2);
+  }
+
+  private void swap(int index1, int index2) {
+    int temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+  }
+
+  private void fillArray(int[] arr) {
+    Arrays.fill(this.arr, 0);
+    for (int i=0; i<arr.length; i++)
+      this.arr[i] = arr[i];
+  }
+
+  private void percolateUp(int i) {
+    while (i != 0 && arr[parent(i)] < arr[i]) {
+      swap(i, parent(i));
+      i = parent(i);
+    }
+  }
+
+  // A.K.A percolateDown(i)
+  private void heapify(int i) {
+    int left = left(i);
+    int right = right(i);
+    int largest = i;
+
+    if (left < size && arr[i] < arr[left])
+      largest = left;
+
+    if (right < size && arr[largest] < arr[right])
+      largest = right;
+
+    if (largest != i) {
+      swap(largest, i);
+      heapify(largest);
+    }
+  }
+
+  private void buildHeap() {
+    int i = (int) Math.floor(size / 2);
+    while (i >= 0) {
+      heapify(i);
+      i--;
+    }
+  }
+  // * ----- END -----
+
+  public void insert(int value) {
+    if (size == capacity)
+      throw new IndexOutOfBoundsException("Overflow: cannot insert value");
+
+    ++size;
+    int i = size - 1;
+    arr[i] = value;
+    percolateUp(i);
+  }
+
+  public int extractMax() {
+    int max = arr[0];
+    arr[0] = arr[size - 1];
+    arr[size - 1] = 0;
+    --size;
+    
+    heapify(0);
+    return max;
+  }
+
+  public int getMax() {
+    return arr[0];
+  }
+
+  public void increaseKey(int i, int newVal) {
+    arr[i] = newVal;
+    percolateUp(i);
+  }
+
+  public void decreaseKey(int i, int newVal) {
+    arr[i] = newVal;
+    heapify(i);
+  }
+
+  public int delete(int i) {
+    if (i >= size)
+      throw new IndexOutOfBoundsException("Overflow: Cannot delete key");
+
+    if (i < 0)
+      throw new IndexOutOfBoundsException("Underflow: Cannot delete key");
+
+    int deletedKey = arr[i];
+    increaseKey(i, Integer.MAX_VALUE);
+    extractMax();
+    return deletedKey;
+  }
+
+  public void display() {
+    System.out.print("\nHeap -> ");
+    for (int i=0; i<size; i++)
+        System.out.print(arr[i] + " ");
+
+    System.out.println();
+  }
+}
---
+++ Trees/MaxHeap/MaxHeapTest.java
@@ -0,0 +1,97 @@
+package Trees.MaxHeap;
+
+public class MaxHeapTest {
+  public static void main(String[] args) {
+    MaxHeap heap1 = new MaxHeap(11);
+
+    heap1.insert(3); 
+    heap1.insert(2); 
+    heap1.insert(-1);
+    heap1.insert(10);
+    heap1.insert(15); 
+    heap1.insert(5); 
+    heap1.insert(4); 
+    heap1.insert(45);
+    heap1.insert(-125);
+    heap1.insert(25);
+    heap1.insert(57);
+
+    heap1.display();
+
+    // should be 11
+    System.out.println("Current capacity: " + heap1.capacity());
+    
+    // should be 11
+    System.out.println("Current capacity: " + heap1.size());
+
+    // should be 57
+    System.out.println("Max extracted: " + heap1.extractMax());
+
+    // should be 45
+    System.out.println("Max value (not extracted): " + heap1.getMax());
+
+    heap1.decreaseKey(2, 1);
+
+    // should be 45
+    System.out.println("Max extracted: " + heap1.extractMax());
+
+    heap1.display();
+    
+    // should be 11
+    System.out.println("Current capacity: " + heap1.capacity());
+
+    // should be 9
+    System.out.println("Current capacity: " + heap1.size());
+
+    // should be -1
+    System.out.println("Value deleted: " + heap1.delete(5));
+
+    // should be 4
+    System.out.println("Value deleted: " + heap1.delete(2));
+
+    // should be 3
+    System.out.println("Value deleted: " + heap1.delete(4));
+
+    heap1.display();
+    
+    // should be 11
+    System.out.println("Current capacity: " + heap1.capacity());
+
+    // should be 6
+    System.out.println("Current capacity: " + heap1.size());
+
+
+
+    MaxHeap heap2 = new MaxHeap(11, new int[] {10, 9, 8, 26, -1});
+    
+    heap2.display();
+    
+    // should be 11
+    System.out.println("Current capacity: " + heap2.capacity());
+
+    // should be 5
+    System.out.println("Current capacity: " + heap2.size());
+
+    // should be 26
+    System.out.println("Max extracted: " + heap2.extractMax());
+
+    heap2.display();
+
+    // should be 8
+    System.out.println("Value deleted: " + heap2.delete(2));
+    
+    // should be 9
+    System.out.println("Value deleted: " + heap2.delete(1));
+
+    heap2.display();
+    
+    // should be 11
+    System.out.println("Current capacity: " + heap2.capacity());
+
+    // should be 2
+    System.out.println("Current capacity: " + heap2.size());
+
+    // should be 10
+    System.out.println("Max extracted: " + heap2.extractMax());
+  }
+}
