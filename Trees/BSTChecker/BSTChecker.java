ebc86861cb1f72fab8f91009ceb07a7cb4e2fe1b
alpha037 <subhronil.dutta@gmail.com>
2020-11-30 21:16:46 +0530

    Add: Valid BST Check

---
+++ Trees/BSTChecker/BSTChecker.java
@@ -0,0 +1,46 @@
+package Trees.BSTChecker;
+
+// import java.util.ArrayList;
+
+class TreeNode {
+  int value;
+  TreeNode left;
+  TreeNode right;
+
+  public TreeNode(int value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+public class BSTChecker {
+
+  private static boolean validateBST(
+    // TreeNode root, TreeNode left, TreeNode  right
+    TreeNode root, int min, int max
+  ) {
+
+    // Base Case
+    if (root == null)
+      return true;
+
+    // if (left != null && root.value <= left.value)
+    //   return false;
+
+    // if (right != null && root.value >= right.value)
+    //   return false;
+
+    if (min > root.value || max < root.value)
+      return false;
+
+    return 
+      validateBST(root.left, min, root.value) &&
+      validateBST(root.right, root.value, max);
+  }
+
+  public static boolean validateBST(TreeNode root) {
+
+    return validateBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+  }
+}
---
+++ Trees/BSTChecker/BSTCheckerTest.java
@@ -0,0 +1,25 @@
+package Trees.BSTChecker;
+
+public class BSTCheckerTest {
+  public static void main(String[] args) {
+    
+  //        50
+  //      /    \
+  //    30      70
+  //   /  \    /  \
+  // 20   40  60   80
+
+    TreeNode root = new TreeNode(50);
+
+    root.left = new TreeNode(30);
+    root.left.left = new TreeNode(20);
+    root.left.right = new TreeNode(40);
+
+    root.right = new TreeNode(70);
+    root.right.left = new TreeNode(60);
+    root.right.right = new TreeNode(80);
+
+    // should be true
+    System.out.println(BSTChecker.validateBST(root));
+  }
+}
