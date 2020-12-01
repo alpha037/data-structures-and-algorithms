abc00127d34ee2d8a789defb10e702334c42174a
alpha037 <subhronil.dutta@gmail.com>
2020-12-02 01:40:43 +0530

    Add: Trim a BST based on min and max values

---
+++ Trees/TrimBST/Trimmer.java
@@ -0,0 +1,53 @@
+package Trees.TrimBST;
+
+class TreeNode {
+  int value;
+  TreeNode left;
+  TreeNode right;
+
+  public TreeNode(int value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+
+public class Trimmer {
+
+  public static TreeNode trimBST(TreeNode root, int min, int max) {
+
+    // Base Case
+    if (root == null)
+      return null;
+
+    // If the current value is less than min,
+    // then discard the entire left subtree
+    // return the right subtree as this is a
+    // Binary search tree
+    if (root.value < min) 
+      return root.right;
+
+    // If the current value is greater than max,
+    // then discard the entire right subtree
+    // return the left subtree according to
+    // the BST property
+    if (root.value > max)
+      return root.left;
+
+    // If the current value is in range, then
+    // traverse it's left subtree
+    root.left = trimBST(root.left, min, max);
+    root.right = trimBST(root.right, min, max);
+
+    return root;
+  }
+
+  public static void traverse(TreeNode root) {
+    if (root != null) {
+      traverse(root.left);
+      System.out.print(root.value + " ");
+      traverse(root.right);
+    }
+  }
+}
---
+++ Trees/TrimBST/TrimmerTest.java
@@ -0,0 +1,30 @@
+package Trees.TrimBST;
+
+public class TrimmerTest {
+  public static void main(String[] args) {
+    
+  //        50
+  //      /    \
+  //    30      70
+  //   /  \    /  \
+  // 20   40  60   80
+
+    TreeNode root = new TreeNode(50);
+
+    root.left = new TreeNode(30);
+    root.left.left = new TreeNode(20);
+    root.left.right = new TreeNode(40);
+
+    root.right = new TreeNode(70);
+    root.right.left = new TreeNode(60);
+    root.right.right = new TreeNode(80);
+
+    System.out.println("\nBefore deletion: ");
+    Trimmer.traverse(root);
+
+    root = Trimmer.trimBST(root, 30, 70);
+
+    System.out.println("\nAfter deletion: ");
+    Trimmer.traverse(root);
+  }
+}
