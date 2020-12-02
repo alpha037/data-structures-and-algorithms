b499c701be2cf9755c731d0e217110e0571042c3
alpha037 <subhronil.dutta@gmail.com>
2020-12-02 21:57:44 +0530

    Add: Hash table implementation

---
+++ Searching/HashingImpl/HashTable.java
@@ -0,0 +1,117 @@
+package Searching.HashingImpl;
+
+import java.util.Arrays;
+
+public class HashTable {
+
+  private int size;
+  private int[] slots;
+  private String[] values;
+
+  public HashTable(int size) {
+    this.size = size;
+    fillArrays();
+  }
+
+  // * ---- Getters ----
+
+  public int size() {
+    return this.size;
+  }
+
+  // * ---- Getters END ----
+
+  // * ---- Private helper methods ----
+
+  private void fillArrays() {
+    slots = new int[size];
+    values = new String[size];
+    Arrays.fill(slots, 0);
+    Arrays.fill(values, "");
+  }
+
+
+  private int hash(int key) {
+    return key % size;
+  }
+
+
+  private int rehash(int oldHash) {
+    return (oldHash + 1) % size;
+  }
+
+  // * ---- Private helper methods END ----
+
+  // * ---- Public methods ----
+
+  public void put(int key, String value) {
+    int hash = hash(key);
+
+    if (slots[hash] == 0) {
+      slots[hash] = key;
+      values[hash] = value;
+    }
+    else {
+      int rehash = rehash(hash);
+      while (slots[rehash] != 0 && hash != rehash)
+        rehash = rehash(rehash);
+
+      if (slots[rehash] == key)
+        values[rehash] = value;
+
+      else {
+        slots[rehash] = key;
+        values[rehash] = value;
+      }
+    }
+  }
+
+
+  public String get(int key) {
+    int hash = hash(key);
+
+    if (slots[hash] == key)
+      return values[hash];
+
+    int rehash = rehash(hash);
+    while (rehash != hash && slots[rehash] != key)
+      rehash = rehash(rehash);
+
+    return slots[rehash] == key ? values[rehash] : null;
+  }
+
+
+  public String remove(int key) {
+    int hash = hash(key);
+
+    if (slots[hash] == key) {
+      slots[hash] = 0;
+      return values[hash];
+    }
+
+    int rehash = rehash(hash);
+    while (rehash != hash && slots[rehash] != key)
+      rehash = rehash(rehash);
+
+    if (slots[rehash] != key)
+      return null;
+
+    slots[rehash] = 0;
+    return values[rehash];
+  }
+
+
+  public boolean containsKey(int key) {
+    int hash = hash(key);
+    
+    if (slots[hash] == key)
+      return true;
+
+    int rehash = rehash(hash);
+    while (rehash != hash && slots[rehash] != key)
+      rehash = rehash(rehash);
+
+    return slots[rehash] == key;
+  }
+}
+  // * ---- Public methods END ----
---
+++ Searching/HashingImpl/HashTableTest.java
@@ -0,0 +1,34 @@
+package Searching.HashingImpl;
+
+public class HashTableTest {
+  public static void main(String[] args) {
+    
+    HashTable map = new HashTable(10);
+
+    map.put(1, "100");
+    map.put(2, "200");
+    map.put(3, "300");
+    map.put(4, "400");
+
+    // 10
+    System.out.println("Size: " + map.size());
+
+    // should be 200
+    System.out.println("\nValue at key 2: " + map.get(2));
+
+    // should be 400
+    System.out.println("\nValue at key 4: " + map.get(4));
+
+    // should be 400
+    System.out.println("\nValue deleted: " + map.remove(4));
+
+    // should be false
+    System.out.println("\nContains key 4: " + map.containsKey(4));
+    
+    // should be true
+    System.out.println("\nContains key 2: " + map.containsKey(2));
+
+    // should be null
+    System.out.println("\nValue at key 5: " + map.get(5));
+  }
+}
