492776e55294b5b6f68d3acf79bc4648661384ef
alpha037 <subhronil.dutta@gmail.com>
2020-12-04 20:31:27 +0530

    Add: Selection sort

---
+++ Sorting/SelectionSort/Selection.java
@@ -0,0 +1,28 @@
+package Sorting.SelectionSort;
+
+public class Selection {
+
+  public static int[] sort(int[] arr) {
+
+    if (arr.length <= 1)
+      return arr;
+
+    int n = arr.length;
+
+    for (int i=0; i<n-1; i++) {
+      int min = i;
+
+      for (int j=i+1; j<n; j++)
+        if (arr[j] < arr[min])
+          min = j;
+
+      if (min != i) {
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+      }
+    }
+
+    return arr;
+  }
+}
---
+++ Sorting/SelectionSort/SelectionTest.java
@@ -0,0 +1,12 @@
+package Sorting.SelectionSort;
+
+public class SelectionTest {
+  public static void main(String[] args) {
+
+    int[] res = Selection.sort(new int[] {20, -1, 15, 124, -56});
+
+    // should be -56, -1, 15, 20, 124
+    for (int el : res)
+      System.out.print(el + " ");
+  }
+}
