35775ac534c9f9265b1d59dd00ffa6f825afc948
alpha037 <subhronil.dutta@gmail.com>
2020-12-04 21:20:00 +0530

    Add: Insertion sort

---
+++ Sorting/InsertionSort/Insertion.java
@@ -0,0 +1,26 @@
+package Sorting.InsertionSort;
+
+public class Insertion {
+
+  public static int[] sort(int[] arr) {
+
+    if (arr.length <= 1)
+      return arr;
+
+    int n = arr.length;
+
+    for (int i=1; i<n; i++) {
+      int currentKey = arr[i];
+      int j = i - 1;
+
+      while (j >= 0 && currentKey < arr[j]) {
+        arr[j + 1] = arr[j];
+        j--;
+      }
+
+      arr[j + 1] = currentKey;
+    }
+
+    return arr;
+  }
+}
---
+++ Sorting/InsertionSort/InsertionTest.java
@@ -0,0 +1,12 @@
+package Sorting.InsertionSort;
+
+public class InsertionTest {
+  public static void main(String[] args) {
+
+    int[] res = Insertion.sort(new int[] {20, -1, 15, 124, -56});
+
+    // should be -56, -1, 15, 20, 124
+    for (int el : res)
+      System.out.print(el + " ");
+  }
+}
