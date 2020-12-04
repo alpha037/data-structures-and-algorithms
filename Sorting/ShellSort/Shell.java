f8c238c4a4cd772d825f443e460d6a8e92711575
alpha037 <subhronil.dutta@gmail.com>
2020-12-05 01:15:33 +0530

    Add: Shell sort

---
+++ Sorting/ShellSort/Shell.java
@@ -0,0 +1,25 @@
+package Sorting.ShellSort;
+
+public class Shell {
+
+  public static int[] sort(int[] arr) {
+
+    if (arr.length <= 1)
+      return arr;
+
+    int n = arr.length;
+
+    for (int gap= n/2; gap > 0; gap /= 2)
+      for (int i=gap; i<n; i++) {
+        int currentKey = arr[i];
+        int j;
+
+        for (j=i; j >= gap && currentKey < arr[j-gap]; j-=gap)
+          arr[j] = arr[j - gap];
+
+        arr[j] = currentKey;
+      }
+
+    return arr;
+  }
+}
