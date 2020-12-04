dad9c1f33c89b5510f51a6d0cc179a1cf75e0dc2
alpha037 <subhronil.dutta@gmail.com>
2020-12-04 19:59:07 +0530

    Add: Bubble sort

---
+++ Sorting/BubbleSort/Bubble.java
@@ -0,0 +1,38 @@
+package Sorting.BubbleSort;
+
+public class Bubble {
+  public static int[] sort(int[] arr) {
+
+    if (arr.length <= 1)
+      return arr;
+
+    int n = arr.length;
+
+    // Usually, bubble sort takes around
+    // O(n^2) time for both sorted and unsorted
+    // arrays. With this check, the algorithm
+    // will take O(n) time for a sorted array
+    boolean isSwapped;
+
+    for (int i=0; i<n-1; i++) {
+      // To check whether any of the 
+      // elements are swapped or not
+      isSwapped = false;
+
+      for (int j=0; j<n-i-1; j++)
+        if (arr[j] > arr[j+1]) {
+          int temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+          isSwapped = true;
+        }
+
+      // If none of the elements are swapped,
+      // then the array is already sorted
+      if (!isSwapped)
+        break;
+    }
+
+    return arr;
+  }
+}
---
+++ Sorting/BubbleSort/BubbleTest.java
@@ -0,0 +1,12 @@
+package Sorting.BubbleSort;
+
+public class BubbleTest {
+  public static void main(String[] args) {
+
+    int[] res = Bubble.sort(new int[] {20, -1, 15, 124, -56});
+
+    // should be -56, -1, 15, 20, 124
+    for (int el : res)
+      System.out.print(el + " ");
+  }
+}
