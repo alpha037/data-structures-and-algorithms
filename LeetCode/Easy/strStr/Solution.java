d224efc74b81774d1ee2df76eb688a946b59aa91
alpha037 <subhronil.dutta@gmail.com>
2020-12-05 11:23:55 +0530

    Add: Leetcode #28 - Implement strStr()

---
+++ LeetCode/Easy/strStr/Solution.java
@@ -0,0 +1,35 @@
+package LeetCode.Easy.strStr;
+
+public class Solution {
+  public int strStr(String haystack, String needle) {
+
+    if (haystack.equals(needle))
+      return 0;
+
+    int len = needle.length();
+    int i;
+
+    for (i=0; i<haystack.length()-len; i++)
+      if (haystack.substring(i, i + len).equals(needle))
+        return i;
+
+    // Check for the last len characters
+    if (haystack.substring(i).equals(needle))
+      return i;
+
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+
+    // should be 2
+    System.out.println(solution.strStr("hello", "ll"));
+
+    // should be -1
+    System.out.println(solution.strStr("aaaaa", "bba"));
+
+    // should be 0
+    System.out.println(solution.strStr("", ""));
+  }
+}
