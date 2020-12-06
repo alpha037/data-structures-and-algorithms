4eee85cf8e44b0d1348799cef35d3ea28f034114
alpha037 <subhronil.dutta@gmail.com>
2020-12-06 21:52:46 +0530

    Add: Heap sort

---
+++ Sorting/HeapSort/Heap.java
@@ -0,0 +1,47 @@
+package Sorting.HeapSort;
+
+public class Heap {
+  private void heapify(int[] arr, int i, int length) {
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    int largest = i;
+
+    if (left < length && arr[left] > arr[largest])
+      largest = left;
+
+    if (right < length && arr[right] > arr[largest])
+      largest = right;
+
+    if (largest != i) {
+      int temp = arr[i];
+      arr[i] = arr[largest];
+      arr[largest] = temp;
+
+      heapify(arr, largest, length);
+    }
+  }
+
+  private void buildHeap(int[] arr, int length) {
+    int i = (int) Math.floor(length / 2);
+    while (i >= 0) {
+      heapify(arr, i, length);
+      --i;
+    }
+  }
+
+  private void heapSort(int[] arr, int length) {
+    buildHeap(arr, length);
+
+    for (int i = length-1; i > 0; --i) {
+      int temp = arr[0];
+      arr[0] = arr[i];
+      arr[i] = temp;
+
+      heapify(arr, 0, i);
+    }
+  }
+
+  public void sort(int[] arr) {
+    heapSort(arr, arr.length);
+  }
+}
---
+++ Sorting/HeapSort/HeapTest.java
@@ -0,0 +1,15 @@
+package Sorting.HeapSort;
+
+public class HeapTest {
+  public static void main(String[] args) {
+    Heap heap = new Heap();
+
+    int[] arr = {10, -12, 56, -147, 0, 21, 45, 65, 788};
+    heap.sort(arr);
+
+    for (int el : arr)
+      System.out.print(el + " ");
+
+    System.out.println();
+  }
+}
