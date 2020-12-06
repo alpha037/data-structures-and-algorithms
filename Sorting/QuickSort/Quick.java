ca6c2cc0c12d9daebcdf429191cdcaed5a98a49c
alpha037 <subhronil.dutta@gmail.com>
2020-12-06 20:21:41 +0530

    Add: Quicksort

---
+++ Sorting/QuickSort/Quick.java
@@ -0,0 +1,36 @@
+package Sorting.QuickSort;
+
+public class Quick {
+
+  public void sort(int[] arr) {
+    quicksort(arr, 0, arr.length - 1);
+  }
+
+  private void quicksort(int[] arr, int low, int high) {
+    if (low < high) {
+
+      int pivot = partition(arr, low, high);
+
+      quicksort(arr, low, pivot - 1);
+      quicksort(arr, pivot + 1, high);
+    }
+  }
+
+  private int partition(int[] arr, int low, int high) {
+    int i = low - 1;
+
+    for (int j=low; j<=(high-1); j++)
+      if (arr[j] <= arr[high]) {
+        ++i;
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return i + 1;
+  }
+}
---
+++ Sorting/QuickSort/QuickTest.java
@@ -0,0 +1,15 @@
+package Sorting.QuickSort;
+
+public class QuickTest {
+  public static void main(String[] args) {
+    Quick quick = new Quick();
+
+    int[] arr = {10, -12, 56, -147, 0, 21, 45, 65, 788};
+    quick.sort(arr);
+
+    for (int el : arr)
+      System.out.print(el + " ");
+
+    System.out.println();
+  }
+}
