02dc3fa1e66b00e674877f3706ac75b13993fe25
alpha037 <subhronil.dutta@gmail.com>
2021-03-04 11:16:07 +0530

    Add: AlgoEx (Easy) - Sorted Squared Array

---
+++ AlgoExSolutions/Easy/SortedSquaredArray/Program.java
@@ -0,0 +1,30 @@
+package AlgoExSolutions.Easy.SortedSquaredArray;
+
+import java.util.*;
+
+/**
+ * * Sorted Squared Array
+ */
+
+class Program {
+
+  public int[] sortedSquaredArray(int[] array) {
+    // Write your code here.
+		int len = array.length, start = 0, end = len - 1, idx = len - 1;
+		int[] res = new int[len];
+		Arrays.fill(res, 0);
+		
+		while (start <= end) {
+			if (Math.abs(array[start]) < Math.abs(array[end])) {
+				res[idx--] = array[end] * array[end];
+				--end;
+			}
+			else {
+				res[idx--] = array[start] * array[start];
+				++start;
+			}
+		}
+		
+    return res;
+  }
+}
