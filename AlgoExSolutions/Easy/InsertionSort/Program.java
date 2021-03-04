15987baf1836da147574c70099fcf4faae455204
alpha037 <subhronil.dutta@gmail.com>
2021-03-04 20:10:43 +0530

    Add: AlgoEx (Easy) - Insertion Sort

---
+++ AlgoExSolutions/Easy/InsertionSort/Program.java
@@ -0,0 +1,29 @@
+package AlgoExSolutions.Easy.InsertionSort;
+
+// import java.util.*;
+
+/**
+ * * Insertion Sort
+ */
+
+class Program {
+  public static int[] insertionSort(int[] array) {
+    // Write your code here.
+		if (array == null || array.length == 0)
+			return new int[] {};
+		
+		for (int i=1; i<array.length; i++) {
+			int j = i;
+			
+			while (j > 0 && array[j] < array[j-1]) {
+				int temp = array[j];
+				array[j] = array[j-1];
+				array[j-1] = temp;
+				
+				--j;
+			}
+		}
+		
+		return array;
+  }
+}
