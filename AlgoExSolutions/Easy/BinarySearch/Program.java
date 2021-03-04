a4da68729131bc63d0d10095c67c88cfb6a02517
alpha037 <subhronil.dutta@gmail.com>
2021-03-04 19:53:40 +0530

    Add: AlgoEx (Easy) - Binary Search

---
+++ AlgoExSolutions/Easy/BinarySearch/Program.java
@@ -0,0 +1,31 @@
+package AlgoExSolutions.Easy.BinarySearch;
+
+// import java.util.*;
+
+/**
+ * * Binary Search
+ */
+
+class Program {
+	private static int binarySearch(int[] array, int left, int right, int target) {
+		if (left > right)
+			return -1;
+		
+		int mid = (left + right) / 2;
+		
+		if (array[mid] == target)
+			return mid;
+		else if (array[mid] < target)
+			return binarySearch(array, mid + 1, right, target);
+		else
+			return binarySearch(array, left, mid - 1, target);
+	}
+	
+  public static int binarySearch(int[] array, int target) {
+    // Write your code here.
+		if (array == null || array.length == 0)
+    	return -1;
+		
+		return binarySearch(array, 0, array.length - 1, target);
+	}
+}
