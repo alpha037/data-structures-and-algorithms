248b7d77d8d7bff193222506958792612e177094
alpha037 <subhronil.dutta@gmail.com>
2021-03-04 20:13:39 +0530

    Add: AlgoEx (Easy) - Selection Sort

---
+++ AlgoExSolutions/Easy/SelectionSort/Program.java
@@ -0,0 +1,29 @@
+package AlgoExSolutions.Easy.SelectionSort;
+
+// import java.util.*;
+
+/**
+ * * Selection Sort
+ */
+
+class Program {
+  public static int[] selectionSort(int[] array) {
+    // Write your code here.
+		if (array == null || array.length == 0)
+    	return new int[] {};
+		
+		for (int i=0; i<array.length-1; i++) {
+			int minIndex = i;
+			
+			for (int j=i+1; j<array.length; j++)
+				if (array[minIndex] >= array[j])
+					minIndex = j;
+			
+			int temp = array[i];
+			array[i] = array[minIndex];
+			array[minIndex] = temp;
+		}
+		
+		return array;
+  }
+}
