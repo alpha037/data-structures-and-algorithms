cc9c27002a0e4b4cc3bc7347756af1efea35f0c3
alpha037 <subhronil.dutta@gmail.com>
2021-03-04 20:22:38 +0530

    Add: AlgoEx (Easy) - Palindrome Check

---
+++ AlgoExSolutions/Easy/PalindromeCheck/Program.java
@@ -0,0 +1,23 @@
+package AlgoExSolutions.Easy.PalindromeCheck;
+
+// import java.util.*;
+
+/**
+ * * Palindrome Check
+ */
+
+class Program {
+  public static boolean isPalindrome(String str) {
+    // Write your code here.
+		if (str == null || str.length() <= 1)
+    	return true;
+		
+		int i = 0, j = str.length() - 1;
+		
+		while (i < j)
+			if (str.charAt(i++) != str.charAt(j--))
+				return false;
+		
+		return true;
+  }
+}
