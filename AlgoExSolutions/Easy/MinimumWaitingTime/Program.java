7d75cbb9e51e4c3c1500a0ccf6c6fbc665f2f2f7
alpha037 <subhronil.dutta@gmail.com>
2021-03-04 19:46:00 +0530

    Add: AlgoEx (Easy) - Minimum Waiting Time

---
+++ AlgoExSolutions/Easy/MinimumWaitingTime/Program.java
@@ -0,0 +1,21 @@
+package AlgoExSolutions.Easy.MinimumWaitingTime;
+
+import java.util.*;
+
+/**
+ * * Minimum Waiting Time
+ */
+
+class Program {
+
+  public int minimumWaitingTime(int[] queries) {
+    // Write your code here.
+		Arrays.sort(queries);
+		int minWaitTime = 0, len = queries.length;
+		
+		for (int idx = 0; idx < len; idx++)
+			minWaitTime += queries[idx] * (len - 1 - idx);
+		
+		return minWaitTime;
+  }
+}
