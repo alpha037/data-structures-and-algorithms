70bdaa7638312d1dd0573d768e1629d2363e168c
alpha037 <subhronil.dutta@gmail.com>
2021-03-04 11:06:24 +0530

    Add: AlgoEx (Easy) - Validate Subsequence

---
+++ AlgoExSolutions/Easy/ValidateSubSequence/Program.java
@@ -0,0 +1,19 @@
+package AlgoExSolutions.Easy.ValidateSubSequence;
+
+import java.util.*;
+
+/**
+ * * Validate Subsequence
+ */
+
+class Program {
+  public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+    // Write your code here.
+		int j = 0;
+		for (int i=0; i<array.size(); i++)
+			if (i < array.size() && j < sequence.size() && array.get(i) == sequence.get(j))
+				++j;
+		
+		return (j == sequence.size());
+  }
+}
