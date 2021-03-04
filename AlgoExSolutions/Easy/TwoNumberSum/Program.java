9663d1b1e336c9f1be06e449b1e6fb00795fce16
alpha037 <subhronil.dutta@gmail.com>
2021-03-04 11:03:51 +0530

    Add: AlgoEx (Easy) - Two Number Sum

---
+++ AlgoExSolutions/Easy/TwoNumberSum/Program.java
@@ -0,0 +1,24 @@
+package AlgoExSolutions.Easy.TwoNumberSum;
+
+import java.util.*;
+
+/**
+ * * Two Number Sum
+ */
+
+class Program {
+  public static int[] twoNumberSum(int[] array, int targetSum) {
+    // Write your code here.
+		HashMap<Integer, Integer> map = new HashMap<>();
+		
+		for (int i=0; i<array.length; i++) {
+			int res = targetSum - array[i];
+			if (map.containsKey(res) && (i != map.get(res)))
+				return new int[] {array[i], res};
+			else
+				map.put(array[i], i);
+		}
+		
+    return new int[0];
+  }
+}
