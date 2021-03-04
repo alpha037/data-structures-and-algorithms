3abb9640a2b4d194b7a396bb4b868b40b11a9ce3
alpha037 <subhronil.dutta@gmail.com>
2021-03-04 19:50:35 +0530

    Add: AlgoEx (Easy) - Nth Fibonacci

---
+++ AlgoExSolutions/Easy/NthFibonacci/Program.java
@@ -0,0 +1,29 @@
+package AlgoExSolutions.Easy.NthFibonacci;
+
+import java.util.*;
+
+/**
+ * * Nth Fibonacci
+ */
+
+class Program {
+	private static int getNthFib(int n, Map<Integer, Integer> map) {
+		if (n == 1)
+			return 0;
+		
+		if (n == 2)
+			return 1;
+		
+		if (map.containsKey(n))
+			return map.get(n);
+		
+		map.put(n, getNthFib(n - 1, map) + getNthFib(n - 2, map));
+		
+		return map.get(n);
+	}
+	
+  public static int getNthFib(int n) {
+    // Write your code here.
+    return getNthFib(n, new HashMap<>());
+  }
+}
