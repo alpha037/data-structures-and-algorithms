ddb3669f66728f4f90ff711cb76837a421b55665
alpha037 <subhronil.dutta@gmail.com>
2021-03-04 20:27:16 +0530

    Add: AlgoEx (Easy) - Generate Document

---
+++ AlgoExSolutions/Easy/GenerateDocument/Program.java
@@ -0,0 +1,28 @@
+package AlgoExSolutions.Easy.GenerateDocument;
+
+import java.util.*;
+
+/**
+ * * Generate Document
+ */
+
+class Program {
+
+  public boolean generateDocument(String characters, String document) {
+    // Write your code here.
+		if (document.equals("")) return true;
+		
+		Map<Character, Integer> availableChars = new HashMap<>();
+		
+		for (char c : characters.toCharArray())
+			availableChars.put(c, availableChars.getOrDefault(c, 0) + 1);
+		
+		for (char c : document.toCharArray()) {
+			if (!availableChars.containsKey(c) || availableChars.get(c) <= 0) return false;
+			
+			availableChars.put(c, availableChars.get(c) - 1);
+		}
+		
+    return true;
+  }
+}
