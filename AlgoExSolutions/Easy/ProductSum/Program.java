00c650867d6035b12a3c2cf9e1c8ec162b59ddb5
alpha037 <subhronil.dutta@gmail.com>
2021-03-04 19:52:41 +0530

    Add: AlgoEx (Easy) - Product Sum

---
+++ AlgoExSolutions/Easy/ProductSum/Program.java
@@ -0,0 +1,30 @@
+package AlgoExSolutions.Easy.ProductSum;
+
+import java.util.*;
+
+/**
+ * * Product Sum 
+ */
+
+class Program {
+  // Tip: You can use `element instanceof ArrayList` to check whether an item
+  // is an array or an integer.
+	public static int productSum(List<Object> array, int depth) {
+		if (array == null || array.size() == 0)
+			return 0;
+		
+		int sum = 0;
+		for (Object val : array)
+			if (val instanceof Integer)
+				sum += (depth * (Integer) val);
+			else
+				sum += (depth * productSum((List<Object>) val, depth + 1));
+		
+		return sum;
+	}
+	
+  public static int productSum(List<Object> array) {
+    // Write your code here.
+		return productSum(array, 1);
+  }
+}
