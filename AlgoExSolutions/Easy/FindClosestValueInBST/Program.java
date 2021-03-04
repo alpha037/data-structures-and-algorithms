c6e0f95dd57c586b5f51ac87b870f46bb39638c9
alpha037 <subhronil.dutta@gmail.com>
2021-03-04 19:39:59 +0530

    Add: AlgoEx (Easy) - Find Closest Value In BST

---
+++ AlgoExSolutions/Easy/FindClosestValueInBST/Program.java
@@ -0,0 +1,39 @@
+package AlgoExSolutions.Easy.FindClosestValueInBST;
+
+// import java.util.*;
+
+/**
+ * * Find Closest Value In BST
+ */
+
+class Program {
+	public static int findClosestValueInBstHelper(BST tree, int target, int closest) {
+		if (tree == null)
+			return closest;
+		
+		if (Math.abs(target - closest) > Math.abs(target - tree.value))
+			closest = tree.value;
+		
+		if (tree.value > target && tree.left != null)
+			return findClosestValueInBstHelper(tree.left, target, closest);
+		
+		else if (tree.value < target && tree.right != null)
+			return findClosestValueInBstHelper(tree.right, target, closest);
+		
+		return closest;
+	}
+	
+  public static int findClosestValueInBst(BST tree, int target) {
+		return findClosestValueInBstHelper(tree, target, tree.value);
+  }
+
+  static class BST {
+    public int value;
+    public BST left;
+    public BST right;
+
+    public BST(int value) {
+      this.value = value;
+    }
+  }
+}
