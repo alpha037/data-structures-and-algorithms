8d577c592907ad9117c88057a7bbdcfc7d97a4f5
alpha037 <subhronil.dutta@gmail.com>
2021-03-04 19:34:32 +0530

    Add: AlgoEx (Easy) - Non-Constructible Change

---
+++ AlgoExSolutions/Easy/NonConstructibleChange/Program.java
@@ -0,0 +1,29 @@
+package AlgoExSolutions.Easy.NonConstructibleChange;
+
+import java.util.*;
+
+/**
+ * * Non-Constructible Change
+ */
+
+class Program {
+
+  public int nonConstructibleChange(int[] coins) {
+    // Write your code here.
+		if (coins == null || coins.length == 0)
+			return 1;
+		
+		Arrays.sort(coins);
+		
+		int currentChange = 0;
+		
+		for (int coin : coins) {
+			if (coin > currentChange + 1)
+				return currentChange + 1;
+			
+			currentChange += coin;
+		}
+		
+		return currentChange + 1;
+  }
+}
