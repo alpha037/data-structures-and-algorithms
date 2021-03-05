9a36b17c916875366bf3bdce610cc6aad6a46ff3
alpha037 <subhronil.dutta@gmail.com>
2021-03-05 20:23:45 +0530

    Add: AlgoEx (Medium) - First Duplicate Value

---
+++ AlgoExSolutions/Medium/FirstDuplicateValue/Program.java
@@ -0,0 +1,24 @@
+package AlgoExSolutions.Medium.FirstDuplicateValue;
+
+// import java.util.*;
+
+/**
+ * * First Duplicate Value
+ */
+
+class Program {
+
+  public int firstDuplicateValue(int[] array) {
+    // Write your code here.
+		
+		for (int idx = 0; idx < array.length; idx++) {
+			int targetIdx = Math.abs(array[idx]) - 1;
+			if (array[targetIdx] < 0)
+				return Math.abs(array[idx]);
+			
+			array[targetIdx] = -array[targetIdx];
+		}
+		
+		return -1;
+  }
+}
