93bb601ce9f655f4c9f0c24b6fd77635abc25191
alpha037 <subhronil.dutta@gmail.com>
2021-03-05 20:30:51 +0530

    Add: AlgoEx (Medium) - Validate BST

---
+++ AlgoExSolutions/Medium/ValidateBST/Program.java
@@ -0,0 +1,39 @@
+package AlgoExSolutions.Medium.ValidateBST;
+
+// import java.util.*;
+
+/**
+ * * Validate BST
+ */
+
+class Program {
+	private static boolean validateBST(BST root, int minValue, int maxValue) {
+		if (root == null)	return true;
+		
+		else if (
+			(minValue != Integer.MIN_VALUE && root.value < minValue) ||
+			(maxValue != Integer.MAX_VALUE && root.value >= maxValue)
+		)
+			return false;
+		
+		else
+			return 
+				validateBST(root.left, minValue, root.value) &&
+				validateBST(root.right, root.value, maxValue);
+	}
+	
+  public static boolean validateBst(BST tree) {
+    // Write your code here.
+		return validateBST(tree, Integer.MIN_VALUE, Integer.MAX_VALUE);
+  }
+
+  static class BST {
+    public int value;
+    public BST left;
+    public BST right;
+
+    public BST(int value) {
+      this.value = value;
+    }
+  }
+}
