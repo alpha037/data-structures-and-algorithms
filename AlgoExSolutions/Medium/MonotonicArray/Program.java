f69338f1ded937138d247d9da88a8daa789f3e6e
alpha037 <subhronil.dutta@gmail.com>
2021-03-05 19:25:32 +0530

    Add: AlgoEx (Medium) - Monotonic Array

---
+++ AlgoExSolutions/Medium/MonotonicArray/Program.java
@@ -0,0 +1,25 @@
+package AlgoExSolutions.Medium.MonotonicArray;
+
+// import java.util.*;
+
+/**
+ * * Monotonic Array
+ */
+
+class Program {
+  public static boolean isMonotonic(int[] array) {
+    // Write your code here.
+		if (array == null || array.length <= 2)	return true;
+		
+		boolean isIncreasing = true, isDecreasing = true;
+		
+		for (int i = 0; i < array.length - 1; i++) {
+			if (array[i] > array[i + 1])
+				isIncreasing = false;
+			else if (array[i] < array[i + 1])
+				isDecreasing = false;
+		}
+		
+		return isIncreasing || isDecreasing;
+  }
+}
