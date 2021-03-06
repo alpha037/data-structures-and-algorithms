8c52199c1214a5965eaa380a1c3991478f200a96
alpha037 <subhronil.dutta@gmail.com>
2021-03-06 10:16:39 +0530

    Add: AlgoEx (Medium) - Balanced Brackets

---
+++ AlgoExSolutions/Medium/BalancedBrackets/Program.java
@@ -0,0 +1,56 @@
+package AlgoExSolutions.Medium.BalancedBrackets;
+
+import java.util.*;
+
+/**
+ * * Balanced Brackets
+ */
+
+class Program {
+	private static boolean isValidBracket(char character) {
+		switch (character) {
+			case '(':
+			case '{':
+			case '[':
+			case ')':
+			case '}':
+			case ']':
+				return true;
+				
+			default: return false;
+		}
+	}
+	
+  public static boolean balancedBrackets(String str) {
+    // Write your code here.		
+		Stack<Character> stack = new Stack<>();
+		
+		for (char character : str.toCharArray()) {
+			if (!isValidBracket(character)) continue;
+			
+			switch (character) {
+				case ')':
+				case '}':
+				case ']':
+					if (stack.isEmpty() || stack.pop() != character) return false;
+					break;
+				
+				case '(':
+					stack.push(')');
+					break;
+					
+				case '{':
+					stack.push('}');
+					break;
+					
+				case '[':
+					stack.push(']');
+					break;
+					
+				default: break;
+			}
+		}
+		
+    return stack.isEmpty();
+  }
+}
