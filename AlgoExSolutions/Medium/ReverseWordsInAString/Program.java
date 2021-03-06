886497e69f21d82a4dae4721fe69e35db6a396cf
alpha037 <subhronil.dutta@gmail.com>
2021-03-06 10:25:58 +0530

    Add: AlgoEx (Medium) - Reverse Words In A String

---
+++ AlgoExSolutions/Medium/ReverseWordsInAString/Program.java
@@ -0,0 +1,31 @@
+package AlgoExSolutions.Medium.ReverseWordsInAString;
+
+import java.util.*;
+
+/**
+ * * Reverse Words In A String
+ */
+
+class Program {
+  public String reverseWordsInString(String string) {
+    // Write your code here.
+		List<String> words = new ArrayList<>();
+		int start = 0;
+		
+		for (int idx = 0; idx < string.length(); ++idx) {
+			if (string.charAt(idx) == ' ') {
+				words.add(string.substring(start, idx));
+				start = idx;
+			}
+			else if (string.charAt(start) == ' ') {
+				words.add(" ");
+				start = idx;
+			}
+		}
+		
+		words.add(string.substring(start));
+		
+		Collections.reverse(words);
+		return String.join("", words);
+  }
+}
