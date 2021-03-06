a0e6131a07f71975d9480af8c7de17bbf1684f43
alpha037 <subhronil.dutta@gmail.com>
2021-03-06 10:22:04 +0530

    Add: AlgoEx (Medium) - Group Anagrams

---
+++ AlgoExSolutions/Medium/GroupAnagrams/Program.java
@@ -0,0 +1,29 @@
+package AlgoExSolutions.Medium.GroupAnagrams;
+
+import java.util.*;
+
+/**
+ * * Group Anagrams
+ */
+
+class Program {
+  public static List<List<String>> groupAnagrams(List<String> words) {
+    // Write your code here.
+		HashMap<String, List<String>> map = new HashMap<>();
+		List<List<String>> res = new ArrayList<>();
+		
+		for (String word : words) {
+			char[] chars = word.toCharArray();
+			Arrays.sort(chars);
+			String sortedWord = new String(chars);
+			
+			if (!map.containsKey(sortedWord))
+				map.put(sortedWord, new ArrayList<>());
+			
+			map.get(sortedWord).add(word);
+		}
+		
+		res.addAll(map.values());
+    return res;
+  }
+}
