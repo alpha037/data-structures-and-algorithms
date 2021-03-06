76b0b72ab49a581a76bb0d911f2fc8aaa912756f
alpha037 <subhronil.dutta@gmail.com>
2021-03-06 10:11:05 +0530

    Add: AlgoEx (Medium) - Search In Sorted Matrix

---
+++ AlgoExSolutions/Medium/SearchInSortedMatrix/Program.java
@@ -0,0 +1,24 @@
+package AlgoExSolutions.Medium.SearchInSortedMatrix;
+
+// import java.util.*;
+
+/**
+ * * Search In Sorted Matrix
+ */
+
+class Program {
+  public static int[] searchInSortedMatrix(int[][] matrix, int target) {
+    // Write your code here.
+		int row = 0, col = matrix[0].length - 1;
+		
+		while (row < matrix.length && col >= 0) {
+			if (matrix[row][col] > target) --col;
+			
+			else if (matrix[row][col] < target) ++row;
+			
+			else return new int[] {row, col};
+		}
+		
+    return new int[] {-1, -1};
+  }
+}
