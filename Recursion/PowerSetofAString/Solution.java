40586046acb34000f05c5e76fcc53d85b61ba02a
alpha037 <subhronil.dutta@gmail.com>
2021-03-07 13:45:30 +0530

    Add: Recursion - Power Set of a String

---
+++ Recursion/PowerSetofAString/Solution.java
@@ -0,0 +1,47 @@
+package Recursion.PowerSetofAString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * * Power Set Of A String
+ */
+
+public class Solution {
+  private void generatePowerSet(String input, String output, List<String> powerSet) {
+    /**
+     * Base Case.
+     * 
+     * If the input string is
+     * empty, then we've found
+     * a potential result
+     */
+    if (input.length() == 0) {
+      powerSet.add(output);
+      return;
+    }
+
+    // First, we pick the first character
+    // of the input and try out all the
+    // possibilities
+    generatePowerSet(input.substring(1), output + input.charAt(0), powerSet);
+
+    // Second, we don't pick the first
+    // character and try out all the
+    // possibilities
+    generatePowerSet(input.substring(1), output, powerSet);
+  }
+
+  public List<String> generatePowerSet(String string) {
+    List<String> powerSet = new ArrayList<>();
+    generatePowerSet(string, "", powerSet);
+
+    return powerSet;
+  }
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+
+    System.out.println(solution.generatePowerSet("abc"));
+  }
+}
