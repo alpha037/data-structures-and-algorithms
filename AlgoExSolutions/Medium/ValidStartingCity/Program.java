33a9b358e9661057547a78af3f1bd6fd727c0739
alpha037 <subhronil.dutta@gmail.com>
2021-03-13 23:00:55 +0530

    Add: AlgoEx (Medium) - Valid Starting City

---
+++ AlgoExSolutions/Medium/ValidStartingCity/Program.java
@@ -0,0 +1,26 @@
+package AlgoExSolutions.Medium.ValidStartingCity;
+
+// import java.util.*;
+
+/**
+ * * Valid Starting City
+ */
+
+class Program {
+
+  public int validStartingCity(int[] distances, int[] fuel, int mpg) {
+    // Write your code here.
+		int startingCityIdx = 0, minFuel = 0, milesTravelled = 0;
+		
+		for (int idx = 1; idx < distances.length; idx++) {
+			milesTravelled += (mpg * fuel[idx - 1]) - distances[idx - 1];
+			
+			if (milesTravelled < minFuel) {
+				minFuel = milesTravelled;
+				startingCityIdx = idx;
+			}
+		}
+		
+		return startingCityIdx;
+  }
+}
