413c7c15bd38df1b786c63f8d5707aa8d92343cb
alpha037 <subhronil.dutta@gmail.com>
2021-03-13 19:13:26 +0530

    Add: AlgoEx (Medium) - Kadane's Algorithm

---
+++ AlgoExSolutions/Medium/KadanesAlgorithm/Program.java
@@ -0,0 +1,21 @@
+package AlgoExSolutions.Medium.KadanesAlgorithm;
+
+// import java.util.*;
+
+/**
+ * * Kadane's Algorithm
+ */
+
+class Program {
+  public static int kadanesAlgorithm(int[] array) {
+    // Write your code here.
+		int currentSum = array[0], maximumSum = array[0];
+		
+		for (int idx = 1; idx < array.length; idx++) {
+			currentSum = Math.max(array[idx], currentSum + array[idx]);
+			maximumSum = Math.max(currentSum, maximumSum);
+		}
+		
+    return maximumSum;
+  }
+}
