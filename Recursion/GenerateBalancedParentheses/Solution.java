a1bc1dfc57ed5a7f24a4d04595839499b2772656
alpha037 <subhronil.dutta@gmail.com>
2021-03-14 21:05:58 +0530

    Add: Recursion - Generate Valid Parentheses

---
+++ Recursion/GenerateBalancedParentheses/Solution.java
@@ -0,0 +1,45 @@
+package Recursion.GenerateBalancedParentheses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * * Generate Valid Parentheses
+ */
+
+public class Solution {
+  private void generateValidParentheses(
+    int open, int close, StringBuilder current, List<String> parentheses
+  ) {
+    if (open == 0 && close == 0) {
+      parentheses.add(current.toString());
+      return;
+    }
+
+    if (open > 0) {
+      current.append("(");
+      generateValidParentheses(open - 1, close, current, parentheses);
+      current.deleteCharAt(current.length() - 1);
+    }
+
+    if (close > open) {
+      current.append(")");
+      generateValidParentheses(open, close - 1, current, parentheses);
+      current.deleteCharAt(current.length() - 1);
+    }
+  }
+
+  public List<String> generateValidParentheses(int n) {
+    List<String> parentheses = new ArrayList<>();
+
+    generateValidParentheses(n, n, new StringBuilder(""), parentheses);
+
+    return parentheses;
+  }
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+
+    System.out.println(solution.generateValidParentheses(3));
+  }
+}
