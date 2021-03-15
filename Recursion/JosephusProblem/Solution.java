fc30b38804d69610a03f7e26ab4d2bc074d6146a
alpha037 <subhronil.dutta@gmail.com>
2021-03-16 00:12:37 +0530

    Add: Recursion - Josephus Problem

---
+++ Recursion/JosephusProblem/Solution.java
@@ -0,0 +1,38 @@
+package Recursion.JosephusProblem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * * Josephus Problem
+ */
+
+public class Solution {
+  private int solveJosephusProblem(List<Integer> soldiers, int k, int index) {
+    /**
+     * Base Case.
+     */
+    if (soldiers.size() == 1)
+      return soldiers.get(0);
+
+    int killIdx = (index + k) % soldiers.size();
+    soldiers.remove(killIdx);
+
+    return solveJosephusProblem(soldiers, k, killIdx);
+  }
+
+  public int solveJosephusProblem(int n, int k) {
+    List<Integer> soldiers = new ArrayList<>();
+
+    for (int idx = 1; idx <= n; idx++)
+      soldiers.add(idx);
+
+    return solveJosephusProblem(soldiers, k - 1, 0);
+  }
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+
+    System.out.println(solution.solveJosephusProblem(14, 2));
+  }
+}
