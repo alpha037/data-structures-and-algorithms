23abd633ea5a0d79c38b819f86bebe443be690ff
alpha037 <subhronil.dutta@gmail.com>
2021-03-16 13:31:34 +0530

    Add: AlgoEx (Medium) - Number Of Ways To Make Change

---
+++ AlgoExSolutions/Medium/NumberofWaysToMakeChange/Program.java
@@ -0,0 +1,25 @@
+package AlgoExSolutions.Medium.NumberofWaysToMakeChange;
+
+import java.util.*;
+
+/**
+ * * Number Of Ways To Make Change
+ */
+
+class Program {
+  public static int numberOfWaysToMakeChange(int n, int[] denoms) {
+    // Write your code here.
+		int[] numberOfWays = new int[n + 1];
+		Arrays.fill(numberOfWays, 0);
+		numberOfWays[0] = 1;
+		
+		for (int denom : denoms) {
+			for (int target = 1; target <= n; target++) {
+				if (target >= denom)
+					numberOfWays[target] += numberOfWays[target - denom];
+			}
+		}
+		
+    return numberOfWays[n];
+  }
+}
