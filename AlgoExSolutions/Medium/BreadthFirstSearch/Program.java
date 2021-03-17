b0dbd91bcc36c700e03d72407cdb60545d3b97b2
alpha037 <subhronil.dutta@gmail.com>
2021-03-18 01:29:58 +0530

    Add: AlgoEx (Medium) - Breadth First Search

---
+++ AlgoExSolutions/Medium/BreadthFirstSearch/Program.java
@@ -0,0 +1,42 @@
+package AlgoExSolutions.Medium.BreadthFirstSearch;
+
+import java.util.*;
+
+/**
+ * * Breadth First Search
+ */
+
+class Program {
+  // Do not edit the class below except
+  // for the breadthFirstSearch method.
+  // Feel free to add new properties
+  // and methods to the class.
+  static class Node {
+    String name;
+    List<Node> children = new ArrayList<Node>();
+
+    public Node(String name) {
+      this.name = name;
+    }
+
+    public List<String> breadthFirstSearch(List<String> array) {
+      // Write your code here.
+			Queue<Node> queue = new LinkedList<>();
+			queue.add(this);
+			
+			while (!queue.isEmpty()) {
+				Node current = queue.poll();
+				array.add(current.name);
+				queue.addAll(current.children);
+			}
+			
+      return array;
+    }
+
+    public Node addChild(String name) {
+      Node child = new Node(name);
+      children.add(child);
+      return this;
+    }
+  }
+}
