7f78de736e8d6831754a9f1d2f0abe05886d0512
alpha037 <subhronil.dutta@gmail.com>
2021-03-18 21:50:04 +0530

    Add: AlgoEx (Medium) - Youngest Common Ancestor

---
+++ AlgoExSolutions/Medium/YoungestCommonManager/Program.java
@@ -0,0 +1,67 @@
+package AlgoExSolutions.Medium.YoungestCommonManager;
+
+// import java.util.*;
+
+/**
+ * * Youngest Common Ancestor
+ */
+
+class Program {
+	private static int depth(AncestralTree topAncestor, AncestralTree descendant) {
+		int depth = 0;
+		
+		while (descendant != topAncestor) {
+			descendant = descendant.ancestor;
+			++depth;
+		}
+		
+		return depth;
+	}
+	
+	private static AncestralTree backtrack(
+		AncestralTree lower, AncestralTree higher, int diff
+	) {
+		while (diff-- > 0) lower = lower.ancestor;
+		
+		while (lower != higher) {
+			lower = lower.ancestor;
+			higher = higher.ancestor;
+		}
+		
+		return lower;
+	}
+  
+  /**
+   * * TC: O(d) | SC: O(1)
+   */
+	public static AncestralTree getYoungestCommonAncestor(
+		AncestralTree topAncestor, AncestralTree descendantOne,
+		AncestralTree descendantTwo
+	) {
+		// Write your code here.
+		int depthOne = depth(topAncestor, descendantOne);
+		int depthTwo =  depth(topAncestor, descendantTwo);
+				
+		if (depthOne > depthTwo)
+			return backtrack(descendantOne, descendantTwo, depthOne - depthTwo);
+		
+		return backtrack(descendantTwo, descendantOne, depthTwo - depthOne);
+  }
+
+  static class AncestralTree {
+    public char name;
+    public AncestralTree ancestor;
+
+    AncestralTree(char name) {
+      this.name = name;
+      this.ancestor = null;
+    }
+
+    // This method is for testing only.
+    void addAsAncestor(AncestralTree[] descendants) {
+      for (AncestralTree descendant : descendants) {
+        descendant.ancestor = this;
+      }
+    }
+  }
+}
