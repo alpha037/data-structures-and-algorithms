a39e1ca8a01a26042760dc82c52f28ae27dd6165
alpha037 <subhronil.dutta@gmail.com>
2021-03-23 21:31:46 +0530

    Add: AlgoEx (Hard) - Find Loop

---
+++ AlgoExSolutions/Hard/FindLoop/Program.java
@@ -0,0 +1,38 @@
+package AlgoExSolutions.Hard.FindLoop;
+
+// import java.util.*;
+
+/**
+ * * Find Loop
+ */
+
+class Program {
+  public static LinkedList findLoop(LinkedList head) {
+    // Write your code here.
+		LinkedList fastTracker = head, slowTracker = head;
+		
+		do {
+			slowTracker = slowTracker.next;
+			fastTracker = fastTracker.next.next;
+			
+		} while (fastTracker != slowTracker);
+		
+		slowTracker = head;
+		
+		while (fastTracker != slowTracker) {
+			slowTracker = slowTracker.next;
+			fastTracker = fastTracker.next;
+		}
+		
+    return slowTracker;
+  }
+
+  static class LinkedList {
+    int value;
+    LinkedList next = null;
+
+    public LinkedList(int value) {
+      this.value = value;
+    }
+  }
+}
