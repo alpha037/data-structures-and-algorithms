93ddfeb5c440de58709b2c2b8255a57d390c49b6
alpha037 <subhronil.dutta@gmail.com>
2021-03-23 21:55:09 +0530

    Add: AlgoEx (Hard) - Reverse Linked List

---
+++ AlgoExSolutions/Hard/ReverseLinkedList/Program.java
@@ -0,0 +1,33 @@
+package AlgoExSolutions.Hard.ReverseLinkedList;
+
+// import java.util.*;
+
+/**
+ * * Reverse Linked List
+ */
+
+class Program {
+  public static LinkedList reverseLinkedList(LinkedList head) {
+    // Write your code here.
+		LinkedList nextNode = head, prevNode = null, currNode = null;
+		
+		while (nextNode != null) {
+			currNode = nextNode;
+			nextNode = nextNode.next;
+			
+			currNode.next = prevNode;
+			prevNode = currNode;
+		}
+		
+    return currNode;
+  }
+
+  static class LinkedList {
+    int value;
+    LinkedList next = null;
+
+    public LinkedList(int value) {
+      this.value = value;
+    }
+  }
+}
