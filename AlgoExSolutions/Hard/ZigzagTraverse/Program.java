6a6ee4037e1c6260b034c700197e8152181c584d
alpha037 <subhronil.dutta@gmail.com>
2021-03-23 20:13:36 +0530

    Add: AlgoEx (Hard) - Zigzag Traverse

---
+++ AlgoExSolutions/Hard/ZigzagTraverse/Program.java
@@ -0,0 +1,55 @@
+package AlgoExSolutions.Hard.ZigzagTraverse;
+
+import java.util.*;
+
+/**
+ * * Zigzag Traverse
+ */
+
+class Program {
+  public static List<Integer> zigzagTraverse(List<List<Integer>> array) {
+    // Write your code here.
+		List<Integer> elements = new ArrayList<>();
+    boolean goingDown = true;
+		int height = array.size(), width = array.get(0).size();
+		int row = 0, col = 0;
+		
+		while (!isOutOfBounds(row, col, height, width)) {
+			elements.add(array.get(row).get(col));
+			
+			if (goingDown) {
+				if (col == 0 || row == height - 1) {
+					goingDown = false;
+					
+					if (row == height - 1) ++col;
+					else ++row;
+				}
+				else {
+					++row;
+					--col;
+				}
+			}
+			else {
+				if (row == 0 || col == width - 1) {
+					goingDown = true;
+					
+					if (col == width - 1) ++row;
+					else ++col;
+				}
+				else {
+					--row;
+					++col;
+				}
+			}
+		}
+		
+		return elements;
+  }
+	
+	private static boolean isOutOfBounds(int row, int col, int height, int width) {
+		return (
+			row < 0 || row >= height ||
+			col < 0 || col >= width
+		);
+	}
+}
