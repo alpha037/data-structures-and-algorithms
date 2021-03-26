385662758a1d21fa61c3fdabeaa1b44248b8f923
alpha037 <subhronil.dutta@gmail.com>
2021-03-27 01:30:08 +0530

    Add: AlgoEx (Hard) - Quick Sort

---
+++ AlgoExSolutions/Hard/QuickSort/Program.java
@@ -0,0 +1,56 @@
+package AlgoExSolutions.Hard.QuickSort;
+
+// import java.util.*;
+
+/**
+ * * Quick Sort
+ */
+
+class Program {
+  /**
+   * * Best: O(n log(n)) time | O(log(n)) space
+   * * Average: O(n log(n)) time | O(log(n)) space
+   * * Worst: O(n^2) time | O(log(n)) space
+   */
+  public static int[] quickSort(int[] array) {
+    // Write your code here.
+		quickSort(array, 0, array.length - 1);
+		return array;
+  }
+	
+	private static void quickSort(int[] array, int low, int high) {
+		while (low < high) {
+			int partitionIdx = partition(array, low, high);
+			boolean isLeftSubArraySmaller = partitionIdx - low < high - partitionIdx;
+			
+			if (isLeftSubArraySmaller) {
+				quickSort(array, low, partitionIdx - 1);
+				low = partitionIdx + 1;
+			}
+			else {
+				quickSort(array, partitionIdx + 1, high);
+				high = partitionIdx - 1;
+			}
+		}
+	}
+	
+	private static int partition(int[] array, int low, int high) {
+		int pivotIdx = high, i = low - 1;
+		
+		for (int j = low; j < high; j++) {
+			if (array[j] <= array[pivotIdx]) {
+				i++;
+				swap(array, i, j);
+			}
+		}
+		
+		swap(array, i + 1, pivotIdx);
+		return i + 1;
+	}
+	
+	private static void swap(int[] array, int index1, int index2) {
+		int temp = array[index1];
+		array[index1] = array[index2];
+		array[index2] = temp;
+	}
+}
