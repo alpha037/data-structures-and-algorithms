ea78e7edf2356541c45daef8fc899eb94010f11c
alpha037 <subhronil.dutta@gmail.com>
2021-03-28 14:54:43 +0530

    Add: AlgoEx (Hard) - Search For Range

---
+++ AlgoExSolutions/Hard/SearchForRange/Program.java
@@ -0,0 +1,63 @@
+package AlgoExSolutions.Hard.SearchForRange;
+
+// import java.util.*;
+
+/**
+ * * Search For Range
+ */
+
+class Program {
+  public static int[] searchForRange(int[] array, int target) {
+    // Write your code here.
+    return new int[] {
+			searchFirst(array, target),
+			searchLast(array, target)
+		};
+  }
+  
+  /**
+   * * TC: O(log n)
+   * * SC: O(1)
+   */
+	private static int searchFirst(int[] array, int target) {
+    int index = -1, low = 0, high = array.length - 1;
+
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+
+      if (target == array[mid]) {
+        index = mid;
+        high = mid - 1;
+      }
+			
+      else if (target < array[mid]) high = mid - 1;
+      
+			else low = mid + 1;
+    }
+
+    return index;
+  }
+
+  /**
+   * * TC: O(log n)
+   * * SC: O(1)
+   */
+  private static int searchLast(int[] array, int target) {
+    int index = -1, low = 0, high = array.length - 1;
+
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+
+      if (target == array[mid]) {
+        index = mid;
+        low = mid + 1;
+      }
+			
+      else if (target < array[mid]) high = mid - 1;
+      
+			else low = mid + 1;
+    }
+
+    return index;
+  }
+}
