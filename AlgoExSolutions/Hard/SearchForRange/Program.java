6deede9fb1483ccf01ecd12752881a09b0b58619
alpha037 <subhronil.dutta@gmail.com>
2021-03-28 16:18:45 +0530

    Mod: AlgoEx (Hard) - Search For Range

--- AlgoExSolutions/Hard/SearchForRange/Program.java
+++ AlgoExSolutions/Hard/SearchForRange/Program.java
@@ -8,14 +8,26 @@ package AlgoExSolutions.Hard.SearchForRange;
 class Program {
   public static int[] searchForRange(int[] array, int target) {
     // Write your code here.
-    return new int[] {searchFirst(array, target), searchLast(array, target)};
+    // int[] range = new int[] {-1, -1};
+
+    // searchForRangeHelper(array, target, 0, array.length - 1, range, true);
+    // searchForRangeHelper(array, target, 0, array.length - 1, range, false);
+
+    // return range;
+
+    return new int[] {
+      searchForRangeHelper(array, target, true),
+      searchForRangeHelper(array, target, false)
+    };
   }
 
   /**
    * * TC: O(log n)
    * * SC: O(1)
    */
-  private static int searchFirst(int[] array, int target) {
+  private static int searchForRangeHelper(
+    int[] array, int target, boolean startRange
+  ) {
     int index = -1, low = 0, high = array.length - 1;
 
     while (low <= high) {
@@ -23,7 +35,10 @@ class Program {
 
       if (target == array[mid]) {
         index = mid;
-        high = mid - 1;
+
+        if (startRange) high = mid - 1;
+        else low = mid + 1;
+
       } else if (target < array[mid]) high = mid - 1;
       else low = mid + 1;
     }
@@ -33,21 +48,32 @@ class Program {
 
   /**
    * * TC: O(log n)
-   * * SC: O(1)
+   * * SC: O(log n)
    */
-  private static int searchLast(int[] array, int target) {
-    int index = -1, low = 0, high = array.length - 1;
+  // private static void searchForRangeHelper(
+  //   int[] array, int target, int low, int high, int[] range, boolean startRange
+  // ) {
+  //   if (low > high) return;
 
-    while (low <= high) {
-      int mid = low + (high - low) / 2;
+  //   int mid = low + (high - low) / 2;
 
-      if (target == array[mid]) {
-        index = mid;
-        low = mid + 1;
-      } else if (target < array[mid]) high = mid - 1;
-      else low = mid + 1;
-    }
+  //   if (array[mid] < target)
+  //     searchForRangeHelper(array, target, mid + 1, high, range, startRange);
+    
+  //   else if (array[mid] > target)
+  //     searchForRangeHelper(array, target, low, mid - 1, range, startRange);
 
-    return index;
-  }
+  //   else {
+  //     if (startRange) {
+  //       if (mid == 0 || array[mid - 1] != target)
+  //         range[0] = mid;
+  //       else searchForRangeHelper(array, target, low, mid - 1, range, startRange);
+  //     }
+  //     else {
+  //       if (mid == array.length - 1 || array[mid + 1] != target)
+  //         range[1] = mid;
+  //       else searchForRangeHelper(array, target, mid + 1, high, range, startRange);
+  //     }
+  //   }
+  // }
 }
