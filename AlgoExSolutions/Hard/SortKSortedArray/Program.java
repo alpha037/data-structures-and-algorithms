3732b4aa00d87cefbdf9cea0f69c96ea2c7a68a2
alpha037 <subhronil.dutta@gmail.com>
2021-04-03 01:42:58 +0530

    Add: AlgoEx (Hard) - Sort K-Sorted Array

---
+++ AlgoExSolutions/Hard/SortKSortedArray/Program.java
@@ -0,0 +1,33 @@
+package AlgoExSolutions.Hard.SortKSortedArray;
+
+import java.util.*;
+
+/**
+ * * Sort K-Sorted Array
+ */
+
+class Program {
+
+  /**
+   * * TC: O(n log(k))
+   * * SC: O(k)
+   */
+  public int[] sortKSortedArray(int[] array, int k) {
+    // Write your code here.		
+		PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+		int len = array.length, insertAt = 0, i = 0;
+				
+		for (; i < Math.min(k + 1, len); i++) minHeap.add(array[i]);
+		
+		while (insertAt < len) {
+			array[insertAt++] = minHeap.poll();
+			
+			if (i < len) {
+				minHeap.add(array[i]);
+				++i;
+			}
+		}
+		
+    return array;
+  }
+}
