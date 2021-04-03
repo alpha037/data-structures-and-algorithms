3d7c20388b527a6eb9e37c7217ed9e606f9549b5
alpha037 <subhronil.dutta@gmail.com>
2021-04-03 14:28:24 +0530

    Add: AlgoEx (Easy) - First Non-Repeating Character

---
+++ AlgoExSolutions/Easy/FirstNonRepeatingCharacter/Program.java
@@ -0,0 +1,26 @@
+package AlgoExSolutions.Easy.FirstNonRepeatingCharacter;
+
+import java.util.*;
+
+/**
+ * * First Non-Repeating Character
+ */
+
+class Program {
+
+  /**
+   * * TC: O(n)
+   * *SC: O(26) ~ O(1)
+   */
+  public int firstNonRepeatingCharacter(String string) {
+    // Write your code here.
+		Map<Character, Integer> map = new HashMap<>();
+		for (char c :  string.toCharArray()) map.put(c, map.getOrDefault(c, 0) + 1);
+		
+		for (int i = 0; i < string.length(); i++) {
+			if (map.get(string.charAt(i)) == 1) return i;
+		}
+		
+    return -1;
+  }
+}
