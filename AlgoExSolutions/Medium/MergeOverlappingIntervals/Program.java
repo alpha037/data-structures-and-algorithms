4a580c6d3a16cc6b74dbb6804108230f13a25ce7
alpha037 <subhronil.dutta@gmail.com>
2021-04-03 16:35:17 +0530

    Add: AlgoEx (Medium) - Merge Overlapping Intervals

---
+++ AlgoExSolutions/Medium/MergeOverlappingIntervals/Program.java
@@ -0,0 +1,30 @@
+package AlgoExSolutions.Medium.MergeOverlappingIntervals;
+
+import java.util.*;
+
+/**
+ * * Merge Overlapping Intervals
+ */
+
+class Program {
+
+  public int[][] mergeOverlappingIntervals(int[][] intervals) {
+    // Write your code here.
+		Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+		List<int[]> mergedIntervals = new ArrayList<>();
+		int[] previous = intervals[0];
+		mergedIntervals.add(previous);
+		
+		for (int i = 1; i < intervals.length; i++) {
+			int[] current = intervals[i];
+			if (current[0] <= previous[1]) 
+				previous[1] = Math.max(current[1], previous[1]);
+			else {
+				previous = current;
+				mergedIntervals.add(previous);
+			}
+		}
+				
+    return mergedIntervals.toArray(new int[mergedIntervals.size()][]);
+  }
+}
