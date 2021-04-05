894bf905d534e7553ba54d75451923de57d26604
alpha037 <subhronil.dutta@gmail.com>
2021-04-05 21:40:59 +0530

    Add: AlgoEx (Easy) - Tandem Bicycle

---
+++ AlgoExSolutions/Easy/TandemBicycle/Program.java
@@ -0,0 +1,44 @@
+package AlgoExSolutions.Easy.TandemBicycle;
+
+import java.util.*;
+
+/**
+ * * Tandem Bicycle
+ */
+
+class Program {
+
+  /**
+   * * TC: O(n log(n))
+   * * SC: O(1)
+   */
+  public int tandemBicycle(
+		int[] redShirtSpeeds, int[] blueShirtSpeeds, boolean fastest
+	) {
+    // Write your code here.
+		int totalSpeed = 0;
+		Arrays.sort(redShirtSpeeds);
+		Arrays.sort(blueShirtSpeeds);
+		
+		if (fastest) reverse(blueShirtSpeeds);
+		
+		for (int i = 0; i < redShirtSpeeds.length;  i++) {
+			int redShirtSpeed = redShirtSpeeds[i], blueShirtSpeed = blueShirtSpeeds[i];
+			totalSpeed += Math.max(redShirtSpeed, blueShirtSpeed);
+		}
+		
+    return totalSpeed;
+  }
+	
+	private void reverse(int[] array) {
+		int start = 0, end = array.length - 1;
+		while (start < end) {
+			int temp = array[start];
+			array[start] = array[end];
+			array[end] = temp;
+			
+			++start;
+			--end;
+		}
+	}
+}
