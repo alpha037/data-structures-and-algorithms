3873ecb6a48ad197438721631a11fea08d2a13ed
alpha037 <subhronil.dutta@gmail.com>
2021-04-16 01:39:56 +0530

    Add: AlgoEx (Medium) - Sort Stack

---
+++ AlgoExSolutions/Medium/SortStack/Program.java
@@ -0,0 +1,45 @@
+package AlgoExSolutions.Medium.SortStack;
+
+import java.util.*;
+
+/**
+ * * Sort Stack
+ */
+
+class Program {
+
+  /**
+   * * TC: O(n^2)
+   * * SC: O(n)
+   */
+  public ArrayList<Integer> sortStack(ArrayList<Integer> stack) {
+    // Write your code here.
+		if (stack.isEmpty()) return stack;
+		
+		sort(stack);
+    return stack;
+  }
+	
+	private void sort(ArrayList<Integer> stack) {
+		if (stack.size() == 1) return;
+		
+		int poppedValue = stack.remove(stack.size() - 1);
+		
+		sort(stack);
+		
+		insert(stack, poppedValue);
+	}
+	
+	private void insert(ArrayList<Integer> stack, int value) {
+		if (stack.isEmpty() || stack.get(stack.size() - 1) <= value) {
+			stack.add(value);
+			return;
+		}
+		
+		int poppedValue = stack.remove(stack.size() - 1);
+		
+		insert(stack, value);
+		
+		stack.add(poppedValue);
+	}
+}
