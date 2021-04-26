abb10b1003b4c4c6dd5f5ecae30154f7880aced1
alpha037 <subhronil.dutta@gmail.com>
2021-04-26 20:58:24 +0530

    Add: Searching - Binary Search

---
+++ Searching/BinarySearch/BinarySearch.java
@@ -0,0 +1,49 @@
+package Searching.BinarySearch;
+
+public class BinarySearch {
+  public static int search(int[] arr, int value) {
+    return binarySearch(arr, value, 0, arr.length - 1);
+  }
+
+  /**
+   * * Iterative Approach
+   * 
+   * * TC: O(log n)
+   * * SC: O(1)
+   */
+  private static int binarySearch(int[] arr, int value, int low, int high) {
+    // Base Case
+    while (low < high) {
+      int mid = low + high - low / 2;
+
+      if (arr[mid] == value)
+        return mid;
+
+      else if (arr[mid] < value)
+        low = mid + 1;
+
+      else
+        high = mid - 1;
+    }
+
+    return -1;
+  }
+
+  /**
+   * * Recursive Approach
+   * 
+   * * TC: O(log n)
+   * * SC: O(log n)
+   */
+  // private static int binarySearch(int[] arr, int value, int low, int high) {
+  //   if (low > high) return -1;
+
+  //   int mid = low + high - low / 2;
+
+  //   if (arr[mid] == value) return mid;
+
+  //   else if (arr[mid] < value) return binarySearch(arr, value, mid + 1, high);
+
+  //   return binarySearch(arr, value, low, mid - 1);
+  // }
+}
---
+++ Searching/BinarySearch/BinarySearchTest.java
@@ -0,0 +1,24 @@
+package Searching.BinarySearch;
+
+public class BinarySearchTest {
+  public static void main(String[] args) {
+    
+    int position = BinarySearch.search(new int[] {2, 6, 8, 10, 12, 13}, 10);
+
+    // should be 3
+    System.out.println(
+      position < 0 ? 
+        "\nValue not found in the array" :
+        "\nValue found at position: " + position
+    );
+    
+    position = BinarySearch.search(new int[] {2, 6, 8, 10, 12, 13}, 20);
+
+    // Not found
+    System.out.println(
+      position < 0 ? 
+        "\nValue not found in the array" :
+        "\nValue found at position: " + position
+    );
+  }
+}
