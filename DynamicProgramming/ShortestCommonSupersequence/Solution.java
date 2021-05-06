3f70f7a7696f9e138b600721004fa5b32c782414
alpha037 <subhronil.dutta@gmail.com>
2021-05-06 20:27:14 +0530

    Mod: Dynamic Programming - Shortest Common Supersequence::Print the final string

--- DynamicProgramming/ShortestCommonSupersequence/Solution.java
+++ DynamicProgramming/ShortestCommonSupersequence/Solution.java
@@ -15,9 +15,9 @@ public class Solution {
     // lcsLength = longestCommonSubsequenceRec(s1.length(), s2.length(), s1.toCharArray(),
     // s2.toCharArray());
 
-    // lcsLength = longestCommonSubsequenceDP(s1, s2);
+    lcsLength = longestCommonSubsequenceDP(s1, s2);
 
-    lcsLength = longestCommonSubsequenceDPSpaceOpt(s1, s2);
+    // lcsLength = longestCommonSubsequenceDPSpaceOpt(s1, s2);
 
     /**
      * ? Length of the shortest supersequence =
@@ -33,22 +33,22 @@ public class Solution {
    * * TC: O(mn)
    * * SC: O(n)
    */
-  private int longestCommonSubsequenceDPSpaceOpt(String s1, String s2) {
-    int m = s1.length(), n = s2.length(), idx = 0;
-    int[][] dp = new int[2][n + 1];
+  // private int longestCommonSubsequenceDPSpaceOpt(String s1, String s2) {
+  //   int m = s1.length(), n = s2.length(), idx = 0;
+  //   int[][] dp = new int[2][n + 1];
 
-    for (int i = 1; i < m + 1; i++) {
-      // Calculate whether the the
-      // current row is even/odd.
-      idx = i & 1;
-      for (int j = 1; j < n + 1; j++) {
-        if (s1.charAt(i - 1) == s2.charAt(j - 1)) dp[idx][j] = 1 + dp[1 - idx][j - 1];
-        else dp[idx][j] = Math.max(dp[1 - idx][j], dp[idx][j - 1]);
-      }
-    }
+  //   for (int i = 1; i < m + 1; i++) {
+  //     // Calculate whether the the
+  //     // current row is even/odd.
+  //     idx = i & 1;
+  //     for (int j = 1; j < n + 1; j++) {
+  //       if (s1.charAt(i - 1) == s2.charAt(j - 1)) dp[idx][j] = 1 + dp[1 - idx][j - 1];
+  //       else dp[idx][j] = Math.max(dp[1 - idx][j], dp[idx][j - 1]);
+  //     }
+  //   }
 
-    return dp[idx][n];
-  }
+  //   return dp[idx][n];
+  // }
 
   /**
    * * Dynamic Programming Approach
@@ -56,19 +56,64 @@ public class Solution {
    * * TC: O(mn)
    * * SC: O(mn)
    */
-  // private int longestCommonSubsequenceDP(String s1, String s2) {
-  //   int m = s1.length(), n = s2.length();
-  //   int[][] dp = new int[m + 1][n + 1];
+  private int longestCommonSubsequenceDP(String s1, String s2) {
+    int m = s1.length(), n = s2.length();
+    int[][] dp = new int[m + 1][n + 1];
 
-  //   for (int i = 1; i < m + 1; i++) {
-  //     for (int j = 1; j < n + 1; j++) {
-  //       if (s1.charAt(i - 1) == s2.charAt(j - 1)) dp[i][j] = 1 + dp[i - 1][j - 1];
-  //       else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-  //     }
-  //   }
+    for (int i = 1; i < m + 1; i++) {
+      for (int j = 1; j < n + 1; j++) {
+        if (s1.charAt(i - 1) == s2.charAt(j - 1)) dp[i][j] = 1 + dp[i - 1][j - 1];
+        else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
 
-  //   return dp[m][n];
-  // }
+    System.out.println(buildSequence(s1, s2, dp));
+
+    return dp[m][n];
+  }
+
+  private String buildSequence(String s1, String s2, int[][] dp) {
+    StringBuilder seq = new StringBuilder();
+    int i = s1.length(), j = s2.length();
+
+    // Since it's a super sequence,
+    // so we need to add all the
+    // characters without repetition
+    while (i > 0 && j > 0) {
+      // If both the characters are the same,
+      // then include the current character
+      // only once
+      if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+        seq.append(String.valueOf(s1.charAt(i - 1)));
+
+        --i;
+        --j;
+      }
+      // Otherwise, go in the direction of the
+      // larger value in the dp matrix while
+      // adding the characters
+      else if (dp[i - 1][j] > dp[i][j - 1]) {
+        seq.append(String.valueOf(s1.charAt(i - 1)));
+        --i;
+      }
+      else {
+        seq.append(String.valueOf(s2.charAt(j - 1)));
+        --j;
+      }
+    }
+
+    // Add any remaining characters
+    // from the first string
+    while (i > 0)
+      seq.append(String.valueOf(s1.charAt(--i)));
+
+    // Add any remaining characters
+    // from the second string
+    while (j > 0)
+      seq.append(String.valueOf(s2.charAt(--j)));
+
+    return seq.reverse().toString();
+  }
 
   /**
    * * Memoization Approach
@@ -116,5 +161,8 @@ public class Solution {
 
     // should be 9
     System.out.println(solution.shortestCommonSupersequence("AGGTAB", "GXTXAYB"));
+
+    // should be 6
+    System.out.println(solution.shortestCommonSupersequence("pear", "peach"));
   }
 }
